610cbfeedacfeeacf61519eab0223bb0542938f0
Kristof Goossens <kgoossens@MacBook-Pro-van-Kristof.local>
2019-04-22 11:08:21 +0200

    added radarr

---
+++ radarr.sh
@@ -0,0 +1,16 @@
+sudo docker stop radarr
+sudo docker rm radarr
+
+docker create \
+  --name=radarr \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Europe/Brussels \
+  -p 7878:7878 \
+  -v /storage/apps/radarr:/config \
+  -v /storage/tv:/movies \
+  -v /storage/downloads:/downloads \
+  --restart unless-stopped \
+  linuxserver/radarr
+
+sudo docker start radarr
